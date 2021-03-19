d01a48bc90de3d942069254a8ab81df0419758c1
jonatasouzac <jonata@lonewolfcorp.com>
2021-03-19 19:46:03 -0300

    Commit inicial

---
+++ src/appl/OneAppl.java
@@ -0,0 +1,211 @@
+package appl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+import core.Message;
+
+public class OneAppl {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		new OneAppl(true);
+	}
+
+	public OneAppl() {
+		PubSubClient client = new PubSubClient();
+		client.startConsole();
+	}
+
+	public OneAppl(boolean flag) throws InterruptedException {
+		PubSubClient joubert = new PubSubClient("localhost", 8081);
+		PubSubClient debora = new PubSubClient("localhost", 8082);
+		PubSubClient jonata = new PubSubClient("localhost", 8083);
+		joubert.subscribe("localhost", 8080);
+		debora.subscribe("localhost", 8080);
+		jonata.subscribe("localhost", 8080);
+		Thread accessOne = new ThreadSincronized(joubert, "x", "joubert", "localhost", 8080);
+		Thread accessTwo = new ThreadSincronized(debora, "x", "jonata", "localhost", 8080);
+		Thread accessThree = new ThreadSincronized(jonata, "x", "joubert", "localhost", 8080);
+		Thread accessFour = new ThreadSincronized(jonata, "x", "debora", "localhost", 8080);
+		// "localhost", 8080);
+
+		accessOne.start();
+		accessTwo.start();
+		accessThree.start();
+		accessFour.start();
+
+		try {
+			accessTwo.join();
+			accessOne.join();
+			accessThree.join();
+			accessFour.join();
+		} catch (Exception e) {
+
+		}
+		/*
+		 * Set<Message> logJoubert = joubert.getLogMessages(); Set<Message> logDebora =
+		 * debora.getLogMessages(); Set<Message> logJonata = jonata.getLogMessages();
+		 * Iterator<Message> it = logJoubert.iterator();
+		 * 
+		 * Message first = it.next(); String content = first.getContent();
+		 * System.out.print(content);
+		 * 
+		 * if (content.equals("joubert_acquire_x")) {
+		 * System.out.print("\nAcessando X\n"); TimeUnit.SECONDS.sleep(1); accessOne =
+		 * new ThreadWrapper(joubert, "joubert_release_x", "localhost", 8080);
+		 * accessOne.start(); try { accessOne.join(); } catch (Exception e) { } } else
+		 * if (content.equals("debora_acquire_x")) {
+		 * System.out.print("\nAcessando X\n"); TimeUnit.SECONDS.sleep(1); accessTwo =
+		 * new ThreadWrapper(debora, "debora_release_x", "localhost", 8080);
+		 * accessTwo.start(); try { accessTwo.join(); } catch (Exception e) { } } else
+		 * if (content.equals("jonata_acquire_x")) {
+		 * System.out.print("\nAcessando X\n"); TimeUnit.SECONDS.sleep(1); accessThree =
+		 * new ThreadWrapper(jonata, "jonata_release_x", "localhost", 8080);
+		 * accessThree.start(); try { accessThree.join(); } catch (Exception e) { } }
+		 * 
+		 * it = logJonata.iterator(); System.out.print("Log Jonata itens: "); while
+		 * (it.hasNext()) { Message aux = it.next(); System.out.print(aux.getContent() +
+		 * aux.getLogId() + " | "); } System.out.println();
+		 * 
+		 * it = logDebora.iterator(); System.out.print("Log Debora itens: "); while
+		 * (it.hasNext()) { Message aux = it.next(); System.out.print(aux.getContent() +
+		 * aux.getLogId() + " | "); } System.out.println();
+		 */
+		joubert.stopPubSubClient();
+		debora.stopPubSubClient();
+		jonata.stopPubSubClient();
+	}
+
+	class ThreadWrapper extends Thread {
+		PubSubClient c;
+		String msg;
+		String host;
+		int port;
+
+		public ThreadWrapper(PubSubClient c, String msg, String host, int port) {
+			this.c = c;
+			this.msg = msg;
+			this.host = host;
+			this.port = port;
+		}
+
+		public void run() {
+			c.publish(msg, host, port);
+		}
+	}
+
+	class ThreadSincronized extends Thread {
+		PubSubClient c;
+		String topic;
+		String client_name;
+		String host;
+		int port;
+
+		public ThreadSincronized(PubSubClient c, String topic, String client_name, String host, int port) {
+			this.c = c;
+			this.topic = topic;
+			this.client_name = client_name;
+			this.host = host;
+			this.port = port;
+		}
+
+		public void run() {
+			Thread access = new ThreadWrapper(c, client_name.concat("_acquire_x"), "localhost", 8080);
+			access.start();
+			try {
+				access.join();
+			} catch (Exception e) {
+			}
+			/*
+			 * Thread access = new ThreadWrapper(c, "joubert_acquire_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "jonata_acquire_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "joubert_acquire_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "debora_acquire_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "joubert_release_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "jonata_release_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 * 
+			 * access = new ThreadWrapper(c, "joubert_release_x", "localhost", 8080);
+			 * access.start(); try { access.join(); } catch (Exception e) { }
+			 */
+
+			while (true) {
+				try {
+					Thread.currentThread().sleep(1000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} // 1 segundo
+
+				Set<Message> log = c.getLogMessages();
+				ArrayList<String> log2 = new ArrayList<String>();
+				Iterator<Message> it = log.iterator();
+				while (it.hasNext()) {
+					Message i = it.next();
+					String content = i.getContent();
+					log2.add(content);
+				}
+				it = log.iterator();
+				while (it.hasNext()) {
+					Message i = it.next();
+					String content = i.getContent();
+					String[] parts = content.split("_");
+					if (parts[1].equals("release")) {
+						Iterator<Message> it2 = log.iterator();
+						while (it2.hasNext()) {
+							Message j = it2.next();
+							String content2 = j.getContent();
+							String[] parts2 = content2.split("_");
+
+							if (parts2[0].equals(parts[0])) {
+								log2.remove(content2);
+								break;
+							}
+						}
+					}
+				}
+
+				Iterator<String> it2 = log2.iterator();
+				String first = it2.next();
+
+				if (first.equals(client_name + "_acquire_x")) {
+					
+					System.out.print("\nAcessando X\n");
+					/*
+					 * try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e1) { // TODO
+					 * Auto-generated catch block e1.printStackTrace(); }
+					 */
+					access = new ThreadWrapper(c, client_name.concat("_release_x"), "localhost", 8080);
+					access.start();
+					try {
+						access.join();
+					} catch (Exception e) {
+					}
+					break;
+				}
+			}
+			Set<Message> log = c.getLogMessages();
+			Iterator<Message> it = log.iterator();
+			it = log.iterator();
+			while (it.hasNext()) {
+				Message aux = it.next();
+				System.out.print(aux.getContent() + aux.getLogId() + " | ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
---
+++ src/core/GenericResource.java
@@ -0,0 +1,65 @@
+package core;
+
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class GenericResource<S> {
+	private ConcurrentLinkedQueue<S> registers;
+	private boolean finished, serverFinished;
+	
+	public GenericResource(){
+		this.registers = new ConcurrentLinkedQueue<S>();
+		finished= false;
+		serverFinished=false;
+	}
+	
+	public void putRegister(S register){
+			
+			this.registers.offer(register);
+			wakeup();
+		
+	}	
+	
+	
+	public S getRegister() throws Exception{
+		
+			if(!this.registers.isEmpty())
+				return this.registers.poll();
+			else {
+				if(!finished)
+					suspend();
+				return null;		
+			}		
+	}
+	
+	private synchronized void suspend()throws Exception{
+		wait();
+	}
+	
+	private synchronized void wakeup(){
+		notify();
+	}
+	
+	public int getNumOfRegisters(){
+		return this.registers.size();
+	}
+	
+	public synchronized void setFinished(){
+		this.finished = true;
+		registers.clear();
+		this.notifyAll();		
+	}
+	
+	public boolean isFinished(){
+		return this.finished;
+	}
+	
+	public boolean isStopped(){
+		return serverFinished;
+	}
+	
+	public synchronized void stopServer(){
+		serverFinished = true;
+		this.notifyAll();
+	}
+}
